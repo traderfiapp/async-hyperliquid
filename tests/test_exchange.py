b315cf47b11067d077245ed43f1d340e4529297b
oneforalone <oneforalone@proton.me>
2025-03-14 13:34:37 +0800

    fix: wrong test arguments for update_leverage

--- tests/test_exchange.py
+++ tests/test_exchange.py
@@ -6,8 +6,8 @@ from async_hyper.async_hyper import LimitOrder
 @pytest.mark.asyncio(loop_scope="session")
 async def test_update_leverage(async_hyper):
     leverage = 10
-    btc_name_idx = 0
-    resp: dict = await async_hyper.update_leverage(leverage, btc_name_idx)
+    coin = "BTC"
+    resp: dict = await async_hyper.update_leverage(leverage, coin)
     assert resp["status"] == "ok"
 
 
